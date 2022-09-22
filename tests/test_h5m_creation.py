55caab2037d75a6b07e38e7e05a516dffeaa5500
Jonathan Shimwell <mail@jshimwell.com>
2022-09-22 18:19:47 +0100

    added compound test

--- tests/test_h5m_creation.py
+++ tests/test_h5m_creation.py
@@ -1,6 +1,7 @@
 from cad_to_dagmc import CadToDagmc
-
+from pathlib import Path
 import dagmc_h5m_file_inspector as di
+import cadquery as cq
 
 """
 Tests that check that:
@@ -87,3 +88,33 @@ def test_h5m_with_multi_volume_touching():
         for counter, loop_mat_tag in enumerate(mat_tags, 1):
             tags_dict[counter] = loop_mat_tag
         assert di.get_volumes_and_materials_from_h5m(h5m_file) == tags_dict
+
+
+def test_cq_compound():
+    # make other shapes from the CadQuery examples
+    spline_points = [
+        (2.75, 1.5),
+        (2.5, 1.75),
+        (2.0, 1.5),
+        (1.5, 1.0),
+        (1.0, 1.25),
+        (0.5, 1.0),
+        (0, 1.0),
+    ]
+
+    s = cq.Workplane("XY")
+    r = s.lineTo(3.0, 0).lineTo(3.0, 1.0).spline(spline_points, includeCurrent=True).close()
+    cq_shape_1 = r.extrude(-1)
+
+    s2 = cq.Workplane("XY")
+    r2 = s2.lineTo(3.0, 0).lineTo(3.0, 1.0).spline(spline_points, includeCurrent=True).close()
+    cq_shape_2 = r2.extrude(1)
+
+    compound_of_workplanes = cq.Compound.makeCompound([cq_shape_1.val(), cq_shape_2.val()])
+
+    my_model = CadToDagmc()
+    my_model.add_cadquery_object(object=compound_of_workplanes, material_tags=["mat1", "mat2"])
+    my_model.export_dagmc_h5m_file(filename='compound_dagmc.h5m', max_mesh_size=0.2, min_mesh_size=0.1)
+
+    assert Path('compound_dagmc.h5m').is_file()
+    assert di.get_volumes_and_materials_from_h5m('compound_dagmc.h5m') == {1: "mat1", 2: "mat2"}
