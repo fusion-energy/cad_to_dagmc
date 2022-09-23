1b10f5dd6736cc163410541d10b3922c275d71d0
Jonathan Shimwell <mail@jshimwell.com>
2022-09-23 16:14:50 +0100

    added assembly example

--- examples/cadquery_assembly.py
+++ examples/cadquery_assembly.py
@@ -2,13 +2,13 @@ import cadquery as cq
 from cadquery import Assembly
 from cad_to_dagmc import CadToDagmc
 
-result = sphere = cq.Workplane().sphere(5)
+result = cq.Workplane().sphere(5)
+result2 = sphere = cq.Workplane().moveTo(10, 0).sphere(2)
 
 assembly = Assembly(name="my assembly")
 assembly.add(result)
-assembly.save("examples/single_sphere.stp", exportType="STEP")
-
+assembly.add(result2)
 
 my_model = CadToDagmc()
-my_model.add_stp_file("examples/single_sphere.stp", material_tags=["mat1"])
-my_model.export_dagmc_h5m_file()
+my_model.add_cadquery_object(assembly, material_tags=["mat1", "mat2"])
+my_model.export_dagmc_h5m_file(min_mesh_size=0.5, max_mesh_size=1.)
