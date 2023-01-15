1685777c68cbd11f9640f1286de71a500febb3f0
shimwell <mail@jshimwell.com>
2023-01-15 15:18:58 +0000

    added version

--- src/cad_to_dagmc/__init__.py
+++ src/cad_to_dagmc/__init__.py
@@ -1,1 +1,17 @@
+
+try:
+    # this works for python 3.7 and lower
+    from importlib.metadata import version, PackageNotFoundError
+except (ModuleNotFoundError, ImportError):
+    # this works for python 3.8 and higher
+    from importlib_metadata import version, PackageNotFoundError
+try:
+    __version__ = version("cad_to_dagmc")
+except PackageNotFoundError:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+
+__all__ = ["__version__"]
+
 from .core import CadToDagmc
