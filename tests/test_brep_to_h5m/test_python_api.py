956075c620fb199acdafceac9469a4ab668be042
shimwell <mail@jshimwell.com>
2023-06-17 01:12:26 +0100

    brought some tests back

--- tests/test_brep_to_h5m/test_python_api.py
+++ tests/test_brep_to_h5m/test_python_api.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
-
-from cad_to_dagmc import brep_to_h5m
+import cadquery as cq
+from cad_to_dagmc import brep_to_h5m, CadToDagmc
 
 
 from pathlib import Path
@@ -68,81 +68,66 @@ class TestApiUsage:
 
     #     assert Path("test_brep_file.h5m").is_file()
 
-    # def test_max_mesh_size_impacts_file_size(self):
-    #     """Checks the reducing max_mesh_size value increases the file size"""
+    def test_max_mesh_size_impacts_file_size(self):
+        """Checks the reducing max_mesh_size value increases the file size"""
 
-    #     os.system("rm *.h5m")
-    #     brep_to_h5m(
-    #         brep_filename="tests/test_brep_to_h5m/test_brep_file.brep",
-    #         material_tags=[
-    #             "material_for_volume_1",
-    #             "material_for_volume_2",
-    #             "material_for_volume_3",
-    #             "material_for_volume_4",
-    #             "material_for_volume_5",
-    #             "material_for_volume_6",
-    #         ],
-    #         h5m_filename="test_brep_file_10.h5m",
-    #         min_mesh_size=20,
-    #         max_mesh_size=19,
-    #         mesh_algorithm=1,
-    #     )
-    #     brep_to_h5m(
-    #         brep_filename="tests/test_brep_to_h5m/test_brep_file.brep",
-    #         material_tags=[
-    #             "material_for_volume_1",
-    #             "material_for_volume_2",
-    #             "material_for_volume_3",
-    #             "material_for_volume_4",
-    #             "material_for_volume_5",
-    #             "material_for_volume_6",
-    #         ],
-    #         h5m_filename="test_brep_file_30.h5m",
-    #         min_mesh_size=20,
-    #         max_mesh_size=30,
-    #         mesh_algorithm=1,
-    #     )
+        sphere = cq.Workplane().sphere(100)
 
-    #     assert Path("test_brep_file_10.h5m").is_file()
-    #     assert Path("test_brep_file_30.h5m").is_file()
+        c2d = CadToDagmc()
+        c2d.add_cadquery_object(sphere, ['m1'])
+        os.system("rm *.h5m")
+        c2d.export_dagmc_h5m_file(
+            min_mesh_size=10,
+            max_mesh_size=20,
+            mesh_algorithm=1,
+            filename='test_10_30.h5m'
+        )
+        c2d.export_dagmc_h5m_file(
+            min_mesh_size=20,
+            max_mesh_size=30,
+            mesh_algorithm=1,
+            filename='test_20_30.h5m'
+        )
+        c2d.export_dagmc_h5m_file(
+            min_mesh_size=20,
+            max_mesh_size=25,
+            mesh_algorithm=1,
+            filename='test_20_25.h5m'
+        )
 
-    #     large_file = Path("test_brep_file_10.h5m").stat().st_size
-    #     small_file = Path("test_brep_file_30.h5m").stat().st_size
-    #     assert small_file < large_file
+        assert Path("test_10_30.h5m").is_file()
+        assert Path("test_20_30.h5m").is_file()
+        assert Path("test_20_25.h5m").is_file()
 
-    # def test_h5m_file_tags(self):
-    #     """Checks that a h5m file is created with the correct tags"""
+        large_file = Path("test_10_30.h5m").stat().st_size
+        small_file = Path("test_20_30.h5m").stat().st_size
+        medium_file = Path("test_20_25.h5m").stat().st_size
+        assert small_file < large_file
+        assert small_file < medium_file
 
-    #     test_h5m_filename = "test_dagmc.h5m"
-    #     os.system(f"rm {test_h5m_filename}")
-    #     returned_filename = brep_to_h5m(
-    #         brep_filename="tests/test_brep_to_h5m/test_brep_file.brep",
-    #         material_tags=[
-    #             "mat1",
-    #             "mat2",
-    #             "mat3",
-    #             "mat4",
-    #             "mat5",
-    #             "mat6",
-    #         ],
-    #         h5m_filename=test_h5m_filename,
-    #         min_mesh_size=30,
-    #         max_mesh_size=50,
-    #         mesh_algorithm=1,
-    #     )
+    def test_h5m_file_tags(self):
+        """Checks that a h5m file is created with the correct tags"""
 
-    #     assert Path(test_h5m_filename).is_file()
-    #     assert Path(returned_filename).is_file()
-    #     assert test_h5m_filename == returned_filename
+        sphere1 = cq.Workplane().sphere(20)
+        sphere2 = cq.Workplane().moveTo(100,100).sphere(20)
+        sphere3 = cq.Workplane().moveTo(-100,-100).sphere(20)
 
-    #     assert get_volumes_and_materials_from_h5m(test_h5m_filename) == {
-    #         1: "mat:mat1",
-    #         2: "mat:mat2",
-    #         3: "mat:mat3",
-    #         4: "mat:mat4",
-    #         5: "mat:mat5",
-    #         6: "mat:mat6",
-    #     }
+        c2d = CadToDagmc()
+        c2d.add_cadquery_object(sphere1, ['mat1'])
+        c2d.add_cadquery_object(sphere2, ['mat2'])
+        c2d.add_cadquery_object(sphere3, ['mat3'])
 
-    # TODO add tests to check 7 or more keys results in a value error
-    # because there are only 6 volumes
+        test_h5m_filename = "test_dagmc.h5m"
+        os.system(f"rm {test_h5m_filename}")
+
+        returned_filename = c2d.export_dagmc_h5m_file(test_h5m_filename)
+
+        assert Path(test_h5m_filename).is_file()
+        assert Path(returned_filename).is_file()
+        assert test_h5m_filename == returned_filename
+
+        assert get_volumes_and_materials_from_h5m(test_h5m_filename) == {
+            1: "mat:mat1",
+            2: "mat:mat2",
+            3: "mat:mat3",
+        }
