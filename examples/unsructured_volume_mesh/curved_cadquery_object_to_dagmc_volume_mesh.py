b2db638aa52ef6b832c565b4737a4c5c6f878790
shimwell <shimwell@users.noreply.github.com>
2024-03-02 22:59:37 +0000

    [skip ci] Apply formatting changes

--- examples/unsructured_volume_mesh/curved_cadquery_object_to_dagmc_volume_mesh.py
+++ examples/unsructured_volume_mesh/curved_cadquery_object_to_dagmc_volume_mesh.py
@@ -44,8 +44,4 @@ my_model.add_cadquery_object(
     material_tags=["mat1"],
 )
 
-my_model.export_unstructured_mesh_file(
-    filename="umesh.h5m",
-    max_mesh_size=1,
-    min_mesh_size=0.1
-)
+my_model.export_unstructured_mesh_file(filename="umesh.h5m", max_mesh_size=1, min_mesh_size=0.1)
