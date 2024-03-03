b092db0f2560ccc74436dedb323d39060fa3b986
Jonathan Shimwell <mail@jshimwell.com>
2024-03-03 21:26:38 +0000

    moved material tags to export

--- examples/cadquery_assembly.py
+++ examples/cadquery_assembly.py
@@ -10,5 +10,7 @@ assembly.add(result)
 assembly.add(result2)
 
 my_model = CadToDagmc()
-my_model.add_cadquery_object(assembly, material_tags=["mat1", "mat2"])
-my_model.export_dagmc_h5m_file(min_mesh_size=0.5, max_mesh_size=1.0)
+my_model.add_cadquery_object(assembly)
+my_model.export_dagmc_h5m_file(
+    min_mesh_size=0.5, max_mesh_size=1.0, material_tags=["mat1", "mat2"]
+)
--- examples/multiple_stp_files.py
+++ examples/multiple_stp_files.py
@@ -1,9 +1,10 @@
 from cad_to_dagmc import CadToDagmc
 
 my_model = CadToDagmc()
-my_model.add_stp_file("two_connected_cubes.stp", material_tags=["mat1", "mat2"])
-my_model.add_stp_file("single_sphere.stp", material_tags=["mat3"])
+my_model.add_stp_file("two_connected_cubes.stp")
+my_model.add_stp_file("single_sphere.stp")
 
 my_model.export_dagmc_h5m_file(
-    max_mesh_size=1, min_mesh_size=0.5, implicit_complement_material_tag="air"
+    max_mesh_size=1, min_mesh_size=0.5, implicit_complement_material_tag="air",
+    material_tags=["mat1", "mat2", "mat3"]
 )
