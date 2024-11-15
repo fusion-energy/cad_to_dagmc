ef3df46461b7c7679974ea7015cfea9344dcd58a
shimwell <mail@jshimwell.com>
2024-11-15 16:45:03 +0100

    added scaling example

---
+++ examples/surface_mesh/cadquery_assembly_with_scaled_geometry.py
@@ -0,0 +1,14 @@
+import cadquery as cq
+from cad_to_dagmc import CadToDagmc
+
+result = cq.Workplane().sphere(5)
+result2 = cq.Workplane().moveTo(10, 0).sphere(2)
+
+assembly = cq.Assembly()
+assembly.add(result)
+assembly.add(result2)
+
+my_model = CadToDagmc()
+my_model.add_cadquery_object(cadquery_object=assembly, material_tags=["mat1", "mat2"])
+# scales geometry to makie it 10 times bigger
+my_model.export_dagmc_h5m_file(min_mesh_size=0.5, max_mesh_size=1.0e6, scaling_factor=10.0)
