f3f68b63c8e904d1de1df6c0677c003e3861be7c
Jon Shimwell <mail@jshimwell.com>
2025-11-10 20:39:38 +0000

    printing solid mesh status

--- src/cad_to_dagmc/direct_mesh_plugin.py
+++ src/cad_to_dagmc/direct_mesh_plugin.py
@@ -329,6 +329,7 @@ def to_mesh(
 
     # Step through all of the collected solids and their respective faces to get the vertices
     for solid in solids:
+        print(f"Meshing solid {solid_idx} of {len(solids)}")
         # Reset this each time so that we get the correct number of faces per solid
         face_triangles = {}
 
