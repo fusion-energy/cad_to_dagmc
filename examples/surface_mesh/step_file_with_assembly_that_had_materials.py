3852387954e276174f76740230dc800c364f5208
shimwell <8583900+shimwell@users.noreply.github.com>
2025-12-16 16:59:24 +0000

    [skip ci] Apply formatting changes

--- examples/surface_mesh/step_file_with_assembly_that_had_materials.py
+++ examples/surface_mesh/step_file_with_assembly_that_had_materials.py
@@ -18,13 +18,9 @@ assembly.add(result2, name="result2", material=cq.Material("mat2"))  # note mate
 assembly.export("assembly_step.step")
 
 # currently neither import the assembly material info
-assembly=cq.Assembly().importStep('assembly_step.step')
-workplane = importers.importStep('assembly_step.step')
+assembly = cq.Assembly().importStep("assembly_step.step")
+workplane = importers.importStep("assembly_step.step")
 
 my_model = CadToDagmc()
-my_model.add_stp_file(
-    filename="assembly_step.step"
-)  # note that material tags are not needed here
+my_model.add_stp_file(filename="assembly_step.step")  # note that material tags are not needed here
 my_model.export_dagmc_h5m_file(min_mesh_size=0.5, max_mesh_size=1.0e6)
-
-
